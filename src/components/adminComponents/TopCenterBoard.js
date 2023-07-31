4b44b9253ac1384ada32f9949564c9fd13823725
beomsu1 <128343741+hiYewonMoon1@users.noreply.github.com>
2023-07-31 11:03:44 +0900

    0731

--- src/components/adminComponents/TopCenterBoard.js
+++ src/components/adminComponents/TopCenterBoard.js
@@ -5,7 +5,6 @@ const TopCenterBoard = () => {
     return ( 
     <div>
         <div className='border-2 border-black border-solid rounded-2xl m-10 mb-2 mt-0 flex h-[350px]'>
-
         </div>
     </div>
     );
--- src/router/root.js
+++ src/router/root.js
@@ -1,17 +1,11 @@
 import { createBrowserRouter } from"react-router-dom";
 
 import AdminPage from "../page/admin/AdminPage";
-import FarmerList from "../components/adminComponents/farmer/FarmerListComponent";
-import FarmerListPage from "../page/farmer/FarmerListPage";
 
 const router = createBrowserRouter([
     {
         path: "",
         element: <AdminPage></AdminPage>
-    },
-    {
-        path: "/farmer/list",
-        element: <FarmerListPage></FarmerListPage>
     }
 ]);
 
