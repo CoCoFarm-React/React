ca29f512fed3619256781591a410c16363d317f8
chanunu90 <128343652+chanunu90@users.noreply.github.com>
2023-08-02 18:09:57 +0900

    0802

--- src/components/adminComponents/RightBoard.js
+++ src/components/adminComponents/RightBoard.js
@@ -1,22 +1,79 @@
-const RightBoard = ({getList}) => {
+import { useEffect, useState } from "react";
+import { getBoardList } from "../../api/adminAPI";
 
+
+const initState = {
+    dtoList:[],
+    end:0,
+    start:0,
+    next:false,
+    prev:false,
+    pageNums:[],
+    page:0,
+    size:0,
+    requestDTO: null,
+    cateno: 1
+  }
   
+
+const RightBoard = ({queryObj}) => {
+
+    const [ board , setBoard ] = useState(initState)
     
+    useEffect(() => {
+
+        console.log("queryObj.cateno")
+        console.log(queryObj.cateno)
+
+        getBoardList(queryObj).then(data => {
+
+            console.log(data)
+
+           data.dtoList = data.dtoList.filter(board => board.cateno === 1)
+            console.log(data.dtoList)
+            setBoard(data);
+
+
+
+        })
+
+
+    } , [queryObj])
+
+    console.log("board" + board);
+    console.log(board)
+
 
     return ( 
     <div className="w-1/3">
         <div className='border-2 border-black border-solid rounded-2xl m-2  h-[400px]'>
-            <h1>
-                게시글3
-            </h1>
-            <ul className='text-left pl-4 pt-4'>
-                <li className='text-left pt-2'>
-                    문의 게시판 가이드 - 
-                </li>
-                <li className='text-left pt-2'>
-                    안녕하세요 문의게시판 입니다.
-                </li>
-            </ul>
+            <div className="flex">
+                <div className="ml-2 mt-2">문의게시판</div>
+                <button className="ml-auto mr-3 mt-1 text-gray-400">more+</button>
+            </div>  
+                <div className="m-2 p-2">
+                    <table className="w-full">
+                        <thead className="border-t-2 border-b">
+                        <tr className="bg-[#f6f6f6]">
+                            <th className="w-1/6">번호</th>
+                            <th className="w-4/6">제목</th>
+                            <th className="w-1/6">댓글</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {board.dtoList.map(({bno, title, nickname, rcnt, regDate, cateno}) => 
+                                (
+                                    
+                                        <tr className="hover:bg-[#f6f6f6] transition-all"
+                                        key={bno}
+                                        ><td className="m-2 p-2 transition-all border-b-2 w-1/6 text-center">{bno}</td>    
+                                        <td className="m-2 p-2 transition-all border-b-2 w-4/6">{title}</td> 
+                                        <td className="m-2 p-2 transition-all border-b-2 w-1/6 text-center">{rcnt}</td></tr>)
+                                    )
+                                }
+                        </tbody>
+                    </table>
+                </div>
         </div>
     </div>
     );
