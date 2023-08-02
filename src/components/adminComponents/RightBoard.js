41032310d79ccda86d846906c44c1f5c303410d8
chanunu90 <128343652+chanunu90@users.noreply.github.com>
2023-08-02 18:39:36 +0900

    화면 파이날

--- src/components/adminComponents/RightBoard.js
+++ src/components/adminComponents/RightBoard.js
@@ -22,6 +22,9 @@ const RightBoard = ({queryObj}) => {
     
     useEffect(() => {
 
+        queryObj.cateno = 1
+  
+   
         console.log("queryObj.cateno")
         console.log(queryObj.cateno)
 
@@ -29,9 +32,12 @@ const RightBoard = ({queryObj}) => {
 
             console.log(data)
 
-           data.dtoList = data.dtoList.filter(board => board.cateno === 1)
-            console.log(data.dtoList)
-            setBoard(data);
+            // data.dtoList = data.dtoList.filter(board => board.cateno === 1)
+            // console.log(data.dtoList)
+            
+
+
+            setBoard({...data});
 
 
 
@@ -45,14 +51,14 @@ const RightBoard = ({queryObj}) => {
 
 
     return ( 
-    <div className="w-1/3">
-        <div className='border-2 border-black border-solid rounded-2xl m-2  h-[400px]'>
+    <div className="w-1/3 ">
+        <div className='border-2 border-black border-solid rounded-2xl m-2  h-[400px] overflow-hidden'>
             <div className="flex">
                 <div className="ml-2 mt-2">문의게시판</div>
                 <button className="ml-auto mr-3 mt-1 text-gray-400">more+</button>
             </div>  
                 <div className="m-2 p-2">
-                    <table className="w-full">
+                    <table className="w-full ">
                         <thead className="border-t-2 border-b">
                         <tr className="bg-[#f6f6f6]">
                             <th className="w-1/6">번호</th>
@@ -61,16 +67,17 @@ const RightBoard = ({queryObj}) => {
                         </tr>
                         </thead>
                         <tbody>
-                            {board.dtoList.map(({bno, title, nickname, rcnt, regDate, cateno}) => 
-                                (
-                                    
+                            {(board.dtoList).filter(board => board.cateno === 1).map(({bno, title,  rcnt, regDate} , idx) => 
+                                idx > 6 ? <></> : (
+                                   
                                         <tr className="hover:bg-[#f6f6f6] transition-all"
                                         key={bno}
                                         ><td className="m-2 p-2 transition-all border-b-2 w-1/6 text-center">{bno}</td>    
                                         <td className="m-2 p-2 transition-all border-b-2 w-4/6">{title}</td> 
-                                        <td className="m-2 p-2 transition-all border-b-2 w-1/6 text-center">{rcnt}</td></tr>)
-                                    )
-                                }
+                                        <td className="m-2 p-2 transition-all border-b-2 w-1/6 text-center">{rcnt}</td></tr>
+                                ) 
+                            ) 
+                            }
                         </tbody>
                     </table>
                 </div>
