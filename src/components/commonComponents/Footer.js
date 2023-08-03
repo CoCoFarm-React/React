bc52c5062f4aa053ff81dde3b83ee4a127e7c8f1
beomsu1 <128343741+hiYewonMoon1@users.noreply.github.com>
2023-08-03 14:55:31 +0900

    0803

--- src/components/commonComponents/Footer.js
+++ src/components/commonComponents/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
      <div className="bg-gray-200 text-slate-500 h-16">
-        <div className="m-2 mb-0 pt-2 text-center">
+        <div className="m-2 mb-0 pt-2 text-center text-sm">
 
             <div>
                (유)나이키코리아 대표 Kimberlee Lynn Chang Mendes, 킴벌리 린 창 멘데스 | 서울 강남구 테헤란로 152 강남파이낸스센터 30층 | 통신판매업신고번호 2011-서울강남-03461 | 등록번호 220-88-09068사업자 정보 확인
